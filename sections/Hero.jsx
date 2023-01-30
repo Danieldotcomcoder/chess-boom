92aa70791036b6f7cfec6c58e460cc090c775a00
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-01-30 13:05:24 +0200

    start with hero componenet

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,8 +1,27 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+
 const Hero = () => (
-  <section>
-    Hero section
+  <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hiddern"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    />
+    <div
+      className="flex justify-center items-center flex-col relative z-10"
+    >
+      <motion.h1 variants={textVariant(1.1)}
+        className={styles.heroHeading}
+      >
+        Metaverse
+      </motion.h1>
+    </div>
   </section>
 );
 
