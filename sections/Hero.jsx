35e80b55f3d6fa5f378d880856354d128a014e8d
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-01-31 10:40:19 +0200

    add image inside motion div

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -30,10 +30,22 @@ const Hero = () => (
           <h1 className={styles.heroHeading}>Ness</h1>
         </motion.div>
       </div>
-    
+      <motion.div
+        variants={slideIn('right', 'tween, 0.2,1 ')}
+        className="relative w-full md:-mt-[20px] -mt-[12px]"
+      >
+        <div
+          className="absoulte w-full h-[-300px] hero-gradient rounded-top-[140px] z-[0] -top-[30px]"
+        >
+          <img
+            src="/cover.png"
+            alt="cover"
+            className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+          />
+
+        </div>
 
       </motion.div>
-    
     </motion.div>
   </section>
 );
