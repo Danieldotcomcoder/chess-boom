415354b61544cefb3c967bc1178b96a4743ae128
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-01-31 12:03:42 +0200

    Add stamp and fix divs to fit screen

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -36,15 +36,26 @@ const Hero = () => (
       >
         <div
           className="absoulte w-full h-[-300px] hero-gradient rounded-top-[140px] z-[0] -top-[30px]"
-        >
-          <img
-            src="/cover.png"
-            alt="cover"
-            className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
-          />
+        />
+        <img
+          src="/cover.png"
+          alt="cover"
+          className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+        />
 
-        </div>
+        <a href="explore">
 
+          <div
+            className=" w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10"
+          >
+            <img
+              src="/stamp.png"
+              alt="stamp"
+              className="sm:w-[155px] w-[100px] sm:h[155px] h-[100px] object-contain"
+            />
+
+          </div>
+        </a>
       </motion.div>
     </motion.div>
   </section>
