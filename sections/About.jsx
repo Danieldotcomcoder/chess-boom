bb003e67822b962fee59bd7344c1253b5435dc35
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-01-31 13:44:35 +0200

    Add typing text component in about section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,8 +1,26 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
 const About = () => (
-  <section>
-    About section
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText
+        title="| About Metaverse"
+        textStyles="text-center"
+      />
+    </motion.div>
+    About Section
   </section>
 );
 
